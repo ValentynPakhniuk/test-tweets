cfff9fdd5d08515058e0ea62fa7abdf24be12355
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-21 23:07:35 +0300

    ADDED: title

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,14 +2,20 @@ import { Link } from 'react-router-dom';
 import { TitleNotFound } from './NotFound.styled';
 import { Button } from '../Button/Button.styled';
 import { Container } from '../UsersList/UsersList.styled';
+import { Helmet } from 'react-helmet-async';
 
 export const NotFound = () => {
   return (
-    <Container>
-      <TitleNotFound>Not Found 404</TitleNotFound>
-      <Link to="/">
-        <Button>Go home</Button>
-      </Link>
-    </Container>
+    <>
+      <Helmet>
+        <title>404</title>
+      </Helmet>
+      <Container>
+        <TitleNotFound>Not Found 404</TitleNotFound>
+        <Link to="/">
+          <Button>Go home</Button>
+        </Link>
+      </Container>
+    </>
   );
 };
