e0df1320905c4fa7a2bbcf2fd1cbf3ddcd078361
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-29 22:07:20 +0300

    UPDATE: styled UserItem.jsx

--- src/components/UserItem/UserItem.styled.js
+++ src/components/UserItem/UserItem.styled.js
@@ -1,6 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 380px;
   height: 460px;
   background: linear-gradient(
@@ -9,24 +12,28 @@ export const Item = styled.li`
     #5736a3 54.28%,
     #4b2a99 78.99%
   );
+  background-image: url('../../images/cardBackgroundImage.png'),
+    linear-gradient(114.99deg, #471ca9 -0.99%, #5736a3 54.28%, #4b2a99 78.99%);
+  background-repeat: no-repeat;
+  /* background-position: 28px 36px, 20px 20px, 0 0; */
   box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   border-radius: 20px;
-  & article {
-    flex-basis: 100%;
-    width: 100%;
-    max-width: 380px;
-    height: 460px;
-    flex-basis: calc((100% - 60px) / 3);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+  overflow: hidden;
+  z-index: 0;
+  /* & article { */
+  /* display: flex; */
+  /* flex-direction: column; */
+  /* align-items: center; */
+  /* position: relative; */
+  /* overflow: hidden; */
+  /* z-index: 0; */
+  /* } */
 `;
 
-export const CardBackgroundImage = styled.img`
-  margin-top: 28px;
-  margin-bottom: 18px;
-`;
+// export const CardBackgroundImage = styled.img`
+//   margin-top: 28px;
+//   margin-bottom: 18px;
+// `;
 
 export const BoxLogo = styled.div`
   position: relative;
@@ -46,23 +53,49 @@ export const BoxLineImage = styled.div`
   margin-bottom: 62px;
 `;
 
-export const CardLine = styled.div`
-  width: 380px;
-  height: 8px;
-  background: #ebd8ff;
-  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
-    inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
-`;
-
 export const EllipseAvatar = styled.div`
   width: 80px;
   height: 80px;
   border-radius: 50px;
-  position: absolute;
+  position: relative;
   background: #ebd8ff;
   box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
     inset 0px -2.19582px 4.39163px #ae7be3,
     inset 0px 4.39163px 3.29372px #fbf8ff;
+  margin-top: 175px;
+  margin-bottom: 24px;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 152px;
+    height: 8px;
+    background: #ebd8ff;
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06) inset 0px -1.71846px
+        3.43693px #ae7be3,
+      inset 0px 3.43693px 2.5777px #fbf8ff;
+    top: 50%;
+    left: -150px;
+    transform: translate(0, -50%);
+    z-index: -1;
+  }
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 152px;
+    height: 8px;
+    background: #ebd8ff;
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+      inset 0px -1.71846px 3.43693px #ae7be3,
+      inset 0px 3.43693px 2.5777px #fbf8ff;
+    top: 50%;
+    right: -150px;
+    transform: translate(0, -50%);
+    z-index: -1;
+  }
 `;
 
 export const ImageAvatar = styled.img`
@@ -71,6 +104,9 @@ export const ImageAvatar = styled.img`
   position: absolute;
   border-radius: 50px;
   line-height: 0;
+  bottom: 50%;
+  right: 50%;
+  transform: translate(50%, 50%);
 `;
 
 export const BoxParagraph = styled.div`
